e6166dc83f5677fd0eee647b84b459243379b02e
SOR <3272194354@qq.com>
2025-09-26 11:45:30 +0800

    when snake crush on itself

--- src/Snake.kt
+++ src/Snake.kt
@@ -7,33 +7,34 @@ class Snake(
     fun goAhead(map: MapType, gf: GameFrame) {
         position = toward?.position ?: when (dir) {
             Direction.U -> {
-                if (position.y - 1 < 0 || !map[position.x][position.y - 1].snake) {
+                if (position.y - 1 < 0 || map[position.x][position.y - 1].snake) {
                     gf.gameLose()
                     return
                 } else map[position.x][position.y - 1]
             }
 
             Direction.D -> {
-                if (position.y + 1 >= Settings.y || !map[position.x][position.y + 1].snake) {
+                if (position.y + 1 >= Settings.y || map[position.x][position.y + 1].snake) {
                     gf.gameLose()
                     return
                 } else map[position.x][position.y + 1]
             }
 
             Direction.R -> {
-                if (position.x + 1 >= Settings.x || !map[position.x + 1][position.y].snake) {
+                if (position.x + 1 >= Settings.x || map[position.x + 1][position.y].snake) {
                     gf.gameLose()
                     return
                 } else map[position.x + 1][position.y]
             }
 
             Direction.L -> {
-                if (position.x - 1 < 0 || !map[position.x - 1][position.y].snake) {
+                if (position.x - 1 < 0 || map[position.x - 1][position.y].snake) {
                     gf.gameLose()
                     return
                 } else map[position.x - 1][position.y]
             }
         }
+        
     }
 
     fun addBody(): Snake = Snake(this.position, this)
